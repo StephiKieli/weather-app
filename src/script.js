7970130b5795b8b0c9db94cd29f2fb5f4cd22cc7
StephiKieli <Stephanie.Kielmann@gmx.de>
2022-11-28 19:01:52 +0100

    Optimierung css

--- src/script.js
+++ src/script.js
@@ -19,8 +19,18 @@ let dayToday = document.querySelector("#current-day");
 let timeTodayH = document.querySelector("#current-timeH");
 let timeTodayM = document.querySelector("#current-timeS");
 dayToday.innerHTML = day;
-timeTodayH.innerHTML = dayTimeH;
-timeTodayM.innerHTML = dayTimeM;
+
+if (dayTimeH < 10) {
+  timeTodayH.innerHTML = `0${dayTimeH}`;
+} else {
+  timeTodayH.innerHTML = dayTimeH;
+}
+
+if (dayTimeM < 10) {
+  timeTodayM.innerHTML = `0${dayTimeM}`;
+} else {
+  timeTodayM.innerHTML = dayTimeM;
+}
 
 function showTemp(response) {
   let temp = Math.round(response.data.main.temp);
@@ -33,16 +43,17 @@ function showTemp(response) {
   console.log(response.data.name);
 }
 
-function newTown(event) {
-  event.preventDefault();
-  let inputTown = document.querySelector("#searching-input");
-
+function search(city) {
   let apiKey = "dabf3a7434b4510eb851649c998cdcda";
   let units = "metric";
-  let url = `https://api.openweathermap.org/data/2.5/weather?q=${inputTown.value}&appid=${apiKey}&units=${units}&lang=de`;
-
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}&lang=de`;
   axios.get(url).then(showTemp);
-  console.log(inputTown);
+}
+
+function newTown(event) {
+  event.preventDefault();
+  let city = document.querySelector("#searching-input").value;
+  search(city);
 }
 
 function showPosition(position) {
@@ -65,11 +76,12 @@ let currentTown = document.querySelector("#current");
 searchingTown.addEventListener("submit", newTown);
 currentTown.addEventListener("click", showcurrentTown);
 
+search("Paris");
+
 function showFahrenheit(event) {
   let unitF = document.querySelector("#aktuell-temp");
   unitF.innerHTML = "70";
 }
-
 function showCelsius(event) {
   let unitC = document.querySelector("#aktuell-temp");
   unitC.innerHTML = "22";
